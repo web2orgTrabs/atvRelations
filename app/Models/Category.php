ad2a04d6d247806b3b402851767cbccfad4a9089
caiordm <caiordm08@gmail.com>
2024-09-08 21:44:25 -0300

    configs

---
+++ app/Models/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    // Campo que pode ser atribuído em massa
+    protected $fillable = ['name'];
+
+    // Relacionamento com Books: Uma categoria pode pertencer a muitos livros (many-to-many)
+    public function books()
+    {
+        return $this->belongsToMany(Book::class);
+    }
+}
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    public function run()
+    {
+        Category::create(['name' => 'Romance']);
+        Category::create(['name' => 'Fantasia']);
+        Category::create(['name' => 'Ficção Científica']);
+        Category::create(['name' => 'Ficção Brasileira']);
+        Category::create(['name' => 'Clássico']);
+        Category::create(['name' => 'Suspense']);
+    }
+}
