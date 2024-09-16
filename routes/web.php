74d4a2d457a8f957dbc02262b8988acc7bc7664d
caiordm <caiordm08@gmail.com>
2024-09-15 23:11:32 -0300

    add views

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PublisherController;
+
+// Rotas para Books
+Route::resource('books', BookController::class);
+
+// Rotas para Authors
+Route::resource('authors', AuthorController::class);
+
+// Rotas para Categories
+Route::resource('categories', CategoryController::class);
+
+// Rotas para Publishers
+Route::resource('publishers', PublisherController::class);
